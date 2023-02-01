860e609675bd130000c357b77117c1b6ef2b92a4
ch4570@zlgoon.co.kr <ch4570@zlgoon.co.kr>
2023-02-01 16:52:44 +0900

    [Refactor] 학생 전체 리스트 조회시 비어있는지 여부 체크하는 로직 추가

--- demo/src/main/java/com/example/demo/service/StudentService.java
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
 import java.util.List;
 
 @Service
@@ -31,6 +33,15 @@ public class StudentService {
     * */
     @Transactional(readOnly = true)
     public List<Student> findAllStudent() {
+
+        // 학생 전체 조회
+        List<Student> studentList = studentRepository.findAll();
+
+        // 학생 전체 조회 후 리스트가 비어있다면, 예외를 던진다.
+        if(CollectionUtils.isEmpty(studentList)) {
+            throw new IllegalStateException("조회된 학생 데이터가 없습니다. 확인 후 다시 진행해주시기 바랍니다.");
+        }
+
         return studentRepository.findAll();
     }
 
