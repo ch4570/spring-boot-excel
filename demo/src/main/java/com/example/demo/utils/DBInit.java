95203a415e6ff4e6a86ca08fce7b52596d7a7cab
ch4570@zlgoon.co.kr <ch4570@zlgoon.co.kr>
2023-02-01 16:21:23 +0900

    [Feature] 기능 테스트용 더미 데이터 초기화 클래스 추가

---
+++ demo/src/main/java/com/example/demo/utils/DBInit.java
@@ -0,0 +1,47 @@
+package com.example.demo.utils;
+
+import com.example.demo.domain.dto.StudentDto;
+import com.example.demo.domain.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DBInit {
+
+    private final StudentService studentService;
+    private final StudentRepository repository;
+
+    // Bean이 초기화 될때 한 번 실행되는 메서드
+    @PostConstruct
+    public void init() {
+
+        // 있는 데이터를 전부 삭제
+        repository.deleteAll();
+
+        // 30명의 학생을 insert
+        insertDummyStudent();
+
+        // 학생 리스트를 조회
+        List<Student> list = studentService.findAllStudent();
+
+        // 찾은 학생을 DTO로 변환 후 log로 출력
+        for(Student s : list) {
+            StudentDto dto = s.toEntity();
+            log.info("찾은 학생 = {}", dto);
+        }
+    }
+
+    public void insertDummyStudent() {
+        for(int i=1; i<30; i++) {
+            studentService.saveStudent(Student.builder().name("학생"+i).build());
+        }
+    }
+}
